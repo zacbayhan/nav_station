226ffd893f6a2553f976dfb8dc429ba44898b0a2
zacbayhan <zacbayhan@gmail.com>
2017-11-09 21:54:20 -0500

    merge

--- merge.sh
+++ merge.sh
@@ -3,3 +3,6 @@ echo "current branch is '$CURRENT_BRANCH'"
 
 URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
 echo "Repo url is $URL"
+FROM_BRANCH="development"
+TO_BRANCH="master"
+echo "current branch is '$CURRENT_BRANCH'"
